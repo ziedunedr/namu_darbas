b9b35af0b8d93dac2eb88a1ef75208617c48f984
ziedunedr <48853807+ziedunedr@users.noreply.github.com>
2019-04-24 19:39:22 +0300

    Update funcijos.cpp

--- funcijos.cpp
+++ funcijos.cpp
@@ -1,5 +1,8 @@
 #include "funkcijos.h"
 #include "struktura.h"
+#include <string>
+#include <iomanip>
+#include <iostream>
 
 void meniu()
 {
@@ -11,14 +14,33 @@ void meniu()
        cin>>kr;
     }while (kr=='t' || kr=='T');
 
+	cout << "Jei norite matyti galutini pagal medianna iveskite M; Jei norite matyti galutini pagal vidurki, iveskite V;";
+	cin >> kr;
+
+	/////V.0.1 isvedimas i ekrana
+	std::cout.width(15); std::cout << std::left << "Vardas";
+	std::cout.width(15); std::cout << std::left   << "Pavarde";
+	if (kr == 'M' || kr == 'm')////// V.0.1 tik vienas pasirinktas Galutinis (Vid.) arba Galutinis (Med.) yra išvedamas
+	{
+		std::cout.width(15); std::cout << std::right << "Galutinis (Med.)";
+	}
+	else if (kr == 'v' || kr == 'V')
+	{
+		std::cout.width(15); std::cout << std::right << "Galutinis (Vid.)";
+	}
+	cout << endl;
 
     for (ir i : lentele){
-        cout<<i.var<<endl;
-        cout<<i.pav<<endl;
-        cout<<i.vid<<endl;
-        cout<<i.med<<endl;
-        for (int j : i.paz)
-                    cout<<j<<" | ";
-        cout<<endl;
+		std::cout.width(15); std::cout << std::left << i.var;
+		std::cout.width(15); std::cout << std::left << i.pav;
+		if (kr == 'M' || kr == 'm')
+		{
+			std::cout.width(15); std::cout << std::right << i.med << std::setprecision(2);
+		}
+		else if (kr == 'v' || kr == 'V')
+		{
+			std::cout.width(15); std::cout << std::right << i.vid << std::setprecision(2);
+		}
+		cout << endl;
     }
 }
